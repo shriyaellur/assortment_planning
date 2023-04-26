a4afe85f06ed91fc767b47e52b4364cb5d0bddf3
shriyaellur <97903143+shriyaellur@users.noreply.github.com>
2023-04-26 12:07:44 +0530

    Update Shelf_merchandise.py

--- Shelf_merchandise.py
+++ Shelf_merchandise.py
@@ -42,8 +42,8 @@ rad = st.sidebar.radio('Navigation',['Shelf Space Optimization','Market Basket']
 
 if rad=='Shelf Space Optimization':
     st.header('SHELF SPACE OPTIMIZATION')
-    #imp = Image.open('https://github.com/shriyaellur/assortment_planning/blob/main/Shelf_optimization.jpg','r')
-    #st.image(imp,width=800)
+    imp = Image.open('Shelf_optimization.jpg','r')
+    st.image(imp,width=800)
     
     file1 = st.file_uploader('Upload a file',key='f1')
 
